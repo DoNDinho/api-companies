965bf339d075bfb866ee756cee308dd095311d84
Diego Flores <ext_dfloresr@falabella.cl>
2021-10-13 13:50:58 -0300

    Se modifica llamado a SP LISTAR EMPRESA POR RUT

--- src/data/database/oracle/sql_procedures.js
+++ src/data/database/oracle/sql_procedures.js
@@ -1,10 +1,10 @@
-'use strict'
-const oracledb = require('oracledb')
+'use strict';
+const oracledb = require('oracledb');
 
 const insertCompany = (company) => {
-	const { number, validator } = company.company_identification
-	const { name, email, phone } = company.company_data
-	const { city, street } = company.company_address
+	const { number, validator } = company.company_identification;
+	const { name, email, phone } = company.company_data;
+	const { city, street } = company.company_address;
 
 	return {
 		name: 'SP_INSERTAR_EMPRESA',
@@ -13,8 +13,8 @@ const insertCompany = (company) => {
 			P_CODIGO: { dir: oracledb.BIND_OUT },
 			P_MENSAJE: { dir: oracledb.BIND_OUT }
 		}
-	}
-}
+	};
+};
 
 const getListCompanies = () => {
 	return {
@@ -26,32 +26,34 @@ const getListCompanies = () => {
 			P_CODIGO: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
 			P_MENSAJE: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT }
 		}
-	}
-}
+	};
+};
 
 const getCompanyById = (id) => {
 	return {
 		name: 'SP_LISTAR_EMPRESA_POR_ID',
-		statement: `BEGIN SP_LISTAR_EMPRESA_POR_ID(:P_ID_EMPRESA, :P_RUT_EMPRESA, :P_DV_RUT, :P_NOMBRE_EMPRESA, :P_EMAIL_EMPRESA, :P_TELEFONO_EMPRESA, :P_CIUDAD, :P_DIRECCION, :P_CODIGO, :P_MENSAJE); END;`,
+		statement: `BEGIN SP_LISTAR_EMPRESA_POR_ID(:P_ID_EMPRESA, :P_RUT, :P_DV, :P_NOMBRE, :P_EMAIL, :P_TELEFONO, :P_DIRECCION, :P_NUM_CALLE, :P_ID_COMUNA, :P_DESCRIPCION, :P_CODIGO, :P_MENSAJE); END;`,
 		bind: {
 			P_ID_EMPRESA: { val: parseInt(id), type: oracledb.DB_TYPE_NUMBER, dir: oracledb.BIND_INOUT },
-			P_RUT_EMPRESA: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
-			P_DV_RUT: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
-			P_NOMBRE_EMPRESA: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
-			P_EMAIL_EMPRESA: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
-			P_TELEFONO_EMPRESA: { type: oracledb.DB_TYPE_NUMBER, dir: oracledb.BIND_OUT },
-			P_CIUDAD: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
+			P_RUT: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
+			P_DV: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
+			P_NOMBRE: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
+			P_EMAIL: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
+			P_TELEFONO: { type: oracledb.DB_TYPE_NUMBER, dir: oracledb.BIND_OUT },
 			P_DIRECCION: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
+			P_NUM_CALLE: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
+			P_ID_COMUNA: { type: oracledb.DB_TYPE_NUMBER, dir: oracledb.BIND_OUT },
+			P_DESCRIPCION: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
 			P_CODIGO: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
 			P_MENSAJE: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT }
 		}
-	}
-}
+	};
+};
 
 const updateCompany = (id, company) => {
-	const { number, validator } = company.company_identification
-	const { name, email, phone } = company.company_data
-	const { city, street } = company.company_address
+	const { number, validator } = company.company_identification;
+	const { name, email, phone } = company.company_data;
+	const { city, street } = company.company_address;
 
 	return {
 		name: 'SP_MODIFICAR_EMPRESA',
@@ -68,7 +70,7 @@ const updateCompany = (id, company) => {
 			P_CODIGO: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
 			P_MENSAJE: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT }
 		}
-	}
-}
+	};
+};
 
-module.exports = { insertCompany, getListCompanies, getCompanyById, updateCompany }
+module.exports = { insertCompany, getListCompanies, getCompanyById, updateCompany };
