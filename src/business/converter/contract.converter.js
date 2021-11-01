7286bcd14d9f911c9068134a3aa1e4af1ed3a9be
Diego Flores <ext_dfloresr@falabella.cl>
2021-11-01 18:02:09 -0300

    Se agrega formateo de fecha en respuesta de contratos

--- src/business/converter/contract.converter.js
+++ src/business/converter/contract.converter.js
@@ -1,4 +1,6 @@
 'use strict';
+const { formatDate } = require('../utils/date');
+
 const parseContractResponse = (contract) => {
 	return {
 		id: contract.ID_CONTRATO || contract.P_ID_CONTRATO,
@@ -6,9 +8,9 @@ const parseContractResponse = (contract) => {
 			code: contract.ID_EMPRESA || contract.P_ID_EMPRESA,
 			description: contract.NOMBRE || contract.P_NOMBRE
 		},
-		init_date: contract.FECHAINICIO || contract.P_FECHAINICIO,
-		term_date: contract.FECHATERMINO || contract.P_FECHATERMINO,
-		payment_date: contract.FECHAPAGO || contract.P_FECHAPAGO,
+		init_date: formatDate(contract.FECHAINICIO) || formatDate(contract.P_FECHAINICIO),
+		term_date: formatDate(contract.FECHATERMINO) || formatDate(contract.P_FECHATERMINO),
+		payment_date: formatDate(contract.FECHAPAGO) || formatDate(contract.P_FECHAPAGO),
 		description: contract.DESCRIPCION || contract.P_DESCRIPCION,
 		base_amount: {
 			amount: contract.MONTOBASE || contract.P_MONTOBASE
