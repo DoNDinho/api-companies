407b16107f8837e67fa17a97deebd5853e4da000
Diego Flores <ext_dfloresr@falabella.cl>
2021-11-01 18:10:59 -0300

    Se modifica formateador de fecha

--- src/business/utils/date.js
+++ src/business/utils/date.js
@@ -4,6 +4,7 @@ const formatDate = (fecha) => {
 		return fecha;
 	}
 
+	fecha.setMinutes(fecha.getMinutes() + fecha.getTimezoneOffset());
 	const year = fecha.getFullYear();
 	const month = fecha.getMonth() + 1 < 10 ? `0${fecha.getMonth() + 1}` : fecha.getMonth() + 1;
 	const day = fecha.getDate() < 10 ? `0${fecha.getDate()}` : fecha.getDate();
